dd23658160adb04de47115136c668979c5799b66
Harmanpreet Singh <harmanpreet.preetsingh@gmail.com>
2017-07-13 18:48:58 -0400

    Added Overridden method ToString to the SuperHuman class

--- COMP123_S2017_Lesson9A/COMP123_S2017_Lesson9A/SuperHuman.cs
+++ COMP123_S2017_Lesson9A/COMP123_S2017_Lesson9A/SuperHuman.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 
 /*
- * Name: Tom Tsiliopoulos
- * Date: July 11, 2017
+ * Name: Harmanpreet Singh
+ * Date: July 13, 2017
  * Description: This is the SuperHuman sub class.
- * Version: 0.3 - Added Public AddPower Method
+ * Version: 0.4 - Added Overridden method ToString
  */
 
 namespace COMP123_S2017_Lesson9A
@@ -67,5 +67,29 @@ namespace COMP123_S2017_Lesson9A
                 Console.WriteLine("Name: " + power.Name + " Rank: " + power.Rank);
             }
         }
+
+        /// <summary>
+        /// Overridden the built-in ToString method
+        /// </summary>
+        /// <returns>
+        /// string
+        /// </returns>
+        public override string ToString()
+        {
+            string outputString = "";
+            outputString += "=====================================\n";
+            outputString += "SuperHuman Name: " + this.Name + "\n";
+            outputString += "=====================================\n";
+
+            foreach (Power power in this.Powers)
+            {
+                outputString += "Power: " + power.Name + " Rank: " + power.Rank + "\n";
+            }
+
+            outputString += "=====================================\n";
+
+            return outputString;
+        }
+
     }
 }
