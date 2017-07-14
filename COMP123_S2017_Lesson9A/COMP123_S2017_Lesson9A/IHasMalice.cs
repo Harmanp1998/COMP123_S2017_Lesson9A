b2e643c28b4fb31a8d0feefd6c9a73cc86545eff
Harmanpreet Singh <harmanpreet.preetsingh@gmail.com>
2017-07-13 22:33:16 -0400

    Added IHasMalice Interface

---
+++ COMP123_S2017_Lesson9A/COMP123_S2017_Lesson9A/IHasMalice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Harmanpreet Singh
+ * Date: July 13, 2017
+ * Description: This is an Interface that defines a Karam Property that
+ * must be implemented in any class that subscribes to it.
+ * Version: 0.1 - Created IHasMalice interface
+ */
+namespace COMP123_S2017_Lesson9A
+{
+    /// <summary>
+    /// This is the IHasMalice Interface
+    /// </summary>
+    interface IHasMalice
+    {
+        int Malice { get; set; }
+    }
+}
